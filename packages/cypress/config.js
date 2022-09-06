235407f84fc09b95db252444abfd42b4e178ded6
Stefan Probst <stefanprobst@posteo.de>
2022-09-06 09:00:03 +0200

    fix: add cypress folder to parser options

--- packages/cypress/config.js
+++ packages/cypress/config.js
@@ -1,6 +1,24 @@
+const project = [
+  './cypress/tsconfig.json',
+  './apps/*/cypress/tsconfig.json',
+  './examples/*/cypress/tsconfig.json',
+  './packages/*/cypress/tsconfig.json',
+]
+
 /** @type {import('eslint').Linter.Config} */
 const config = {
   overrides: [
+    {
+      files: [
+        './cypress/**/*.@(ts|tsx)',
+        './apps/*/cypress/**/*.@(ts|tsx)',
+        './examples/*/cypress/**/*.@(ts|tsx)',
+        './packages/*/cypress/**/*.@(ts|tsx)',
+      ],
+      parserOptions: {
+        project,
+      },
+    },
     {
       files: ['**/*.test.e2e.@(ts|tsx)'],
       extends: 'plugin:cypress/recommended',
