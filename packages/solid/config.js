dbb4a907ea9da41e0bbabcd15afc8aaf3dd885b4
Stefan Probst <stefanprobst@posteo.de>
2022-12-08 14:16:20 +0100

    feat: add solid config

---
+++ packages/solid/config.js
@@ -0,0 +1,17 @@
+/** @type {import('eslint').Linter.Config} */
+const config = {
+  overrides: [
+    {
+      files: ['**/*.@(ts|tsx)'],
+      extends: ['plugin:jsx-a11y/recommended', 'plugin:solid/typescript'],
+      rules: {
+        'jsx-a11y/anchor-is-valid': ['error', { components: ['Link'] }],
+        'jsx-a11y/no-autofocus': ['error', { ignoreNonDOM: true }],
+        'jsx-a11y/no-onchange': 'off',
+        'jsx-a11y/no-redundant-roles': ['error', { ul: ['list'], ol: ['list'] }],
+      },
+    },
+  ],
+}
+
+module.exports = config
