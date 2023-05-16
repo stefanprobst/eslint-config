cee4db02b704bfd686f48c9f0e949480556b34aa
Stefan Probst <stefanprobst@posteo.de>
2023-05-16 10:53:25 +0200

    fix: disable no-undef for nuxt auto-imports

---
+++ .changeset/green-socks-refuse.md
@@ -0,0 +1,5 @@
+---
+"@stefanprobst/eslint-config-nuxt": patch
+---
+
+disable no-undef to play nice with nuxt auto-imports
--- packages/nuxt/config.js
+++ packages/nuxt/config.js
@@ -4,6 +4,13 @@ const config = {
 		{
 			files: ["./**/*.vue"],
 			extends: ["plugin:eslint-plugin-nuxt/recommended"],
+			rules: {
+				/**
+				 * Does not work with nuxt auto-imports.
+				 * @see https://github.com/antfu/unplugin-auto-import/issues/3
+				 */
+				"no-undef": "off",
+			},
 		},
 		{
 			files: ["./src/layouts/**/*.vue", "./src/pages/**/*.vue"],
