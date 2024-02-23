6b622f4f80a34bc16e9889db154903904cc004eb
Stefan Probst <stefanprobst@posteo.de>
2024-02-23 07:33:43 +0100

    fix: disable no-unsafe-* rules for astro

---
+++ .changeset/ninety-houses-play.md
@@ -0,0 +1,5 @@
+---
+"@stefanprobst/eslint-config-astro": patch
+---
+
+disable no-unsafe-\* rules for astro
--- packages/astro/config.js
+++ packages/astro/config.js
@@ -29,6 +29,18 @@ const config = {
 				parser: "@typescript-eslint/parser",
 			},
 			rules: {
+				/**
+				 * `astro-eslint-parser` has same issue as `vue-eslint-parser`.
+				 *
+				 * @see https://github.com/vuejs/vue-eslint-parser/issues/104
+				 */
+				"@typescript-eslint/no-unsafe-argument": "off",
+				"@typescript-eslint/no-unsafe-assignment": "off",
+				"@typescript-eslint/no-unsafe-call": "off",
+				"@typescript-eslint/no-unsafe-declaration-merging": "off",
+				"@typescript-eslint/no-unsafe-enum-comparison": "off",
+				"@typescript-eslint/no-unsafe-member-access": "off",
+				"@typescript-eslint/no-unsafe-return": "off",
 				"astro/jsx-a11y/anchor-is-valid": [
 					"error",
 					{ components: ["Link"], aspects: ["invalidHref", "preferButton"] },
